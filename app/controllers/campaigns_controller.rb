ba30bb9452a0b02e1141315c2849fe3c77cb6cc2
Christian Chavarro <christian@clutchprep.com>
2016-12-19 21:18:47 -0500

    Added campaign edit fucntionality

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -19,4 +19,24 @@ class CampaignsController < ApplicationController
 
 		redirect_to root_path
 	end
+
+	def edit
+		@campaign = Campaign.find_by(id: params[:id])
+	end
+
+	def update
+		@campaign = Campaign.find_by(id: params[:id])
+
+		if @campaign.update_attributes(campaign_params)
+			redirect_to root_path
+		else
+			render 'edit'
+		end
+	end
+
+	private
+
+	def campaign_params
+		params.require(:campaign).permit(:user_id, :campaign_phone, :campaign_keyword, :destination_url, :campaign_description, :active, :start_time, :end_time)
+	end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
 	def home
+		@campaigns = current_user.campaigns.order('id ASC')
 	end
 end
